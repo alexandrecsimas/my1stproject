c50f2daefcfa09385bd87dc450298cfa05709190
Alexandre <alexandrecsimas@gmail.com>
2019-04-27 06:46:15 -0300

    removed test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>My1stProject 1</title>
+        <title>My1stProject</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
